b22eadba666a1a2e932d2d27afadbab22af806a3
zhilin010 <zhilin010@gmail.com>
2023-05-06 19:51:41 -0400

    Modify API endpoint to prepare for deployment

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
